58e82c71779325a016c943dad8042155099a438e
Bryan Hoang <bryan@bryanhoang.dev>
2022-05-30 13:01:28 -0400

    style(main.py): format file using black
    Indentation of 2 makes python harder to read and isn't complaint with
    the PEP 8 style guide spec for python. Chose an opinionated formatter
    that's common to come across in open source python projects.

--- main.py
+++ main.py
@@ -1,43 +1,71 @@
 import os
+
 import discord
 
 # Get secrets from environment variables.
 
 # Bot token from https://discord.com/developers/applications.
-TOKEN = os.environ['TOKEN']
+TOKEN = os.environ["TOKEN"]
 # DCP Bank Account Address. e.g., 0x72af1bB78185BB3cCba53dBEdC75FEE875434155
 # Change this address to earn credits into your own wallet.
-BANK_ACCOUNT = os.environ['BANK_ACCOUNT']
+BANK_ACCOUNT = os.environ["BANK_ACCOUNT"]
 
 # URL that the embedded message links to.
 embed_url = f"https://dcp.work/?bankAccount={BANK_ACCOUNT}"
 
 # Connection to discord
-client = discord.Client() 
+client = discord.Client()
 
 # Lists of targeted words/phrases
-hellos = ["hi", "Hi", "hello", "Hello", "sup", "wazzup", "suh", "holla", "howdy", "yo", "good afternoon"]
-goodbyes = ["goodbye", "Goodbye", "bye", "Bye", "later", "goodnight", "till next time", "ta ta", "cya"]
+hellos = [
+    "hi",
+    "Hi",
+    "hello",
+    "Hello",
+    "sup",
+    "wazzup",
+    "suh",
+    "holla",
+    "howdy",
+    "yo",
+    "good afternoon",
+]
+goodbyes = [
+    "goodbye",
+    "Goodbye",
+    "bye",
+    "Bye",
+    "later",
+    "goodnight",
+    "till next time",
+    "ta ta",
+    "cya",
+]
 
-@client.event 
+
+@client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
-  
+    print("We have logged in as {0.user}".format(client))
+
+
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
-  
-  if message.content in hellos:
-    message_prefix = "Hi"
-  elif message.content in goodbyes:
-    message_prefix = "Goodbye"
+    if message.author == client.user:
+        return
 
-  embed = discord.Embed(title="DCP Worker", 
-    url = embed_url,
-    description=f"{message_prefix} fren! This link allocates your unused CPU to be used by the distributed compute protocol. Change the address to earn credits to your account! Why not contribute to a super computer while you're not chatting??", color=0xFF5733)
+    if message.content in hellos:
+        message_prefix = "Hi"
+    elif message.content in goodbyes:
+        message_prefix = "Goodbye"
 
-  await message.channel.send(embed=embed)
+    embed = discord.Embed(
+        title="DCP Worker",
+        url=embed_url,
+        description=f"{message_prefix} fren! This link allocates your unused CPU to be used by the distributed compute protocol. Change the address to earn credits to your account! Why not contribute to a super computer while you're not chatting??",
+        color=0xFF5733,
+    )
+
+    await message.channel.send(embed=embed)
 
 
 client.run(TOKEN)
