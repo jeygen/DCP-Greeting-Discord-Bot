2bcd02c2a404e92b3e0f13fb658f60c608d88c52
Bryan Hoang <bryan@bryanhoang.dev>
2022-05-30 12:57:14 -0400

    refactor(main.py): deduplicate code and use env var for "config" value
    Makes the code more 'DRY'.
    
    A bank account is technically considered a secret, and thus should be
    stored in an environment variable for security.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import os
 import discord
 
+# Get secrets from environment variables.
+
+# Bot token from https://discord.com/developers/applications.
+TOKEN = os.environ['TOKEN']
+# DCP Bank Account Address. e.g., 0x72af1bB78185BB3cCba53dBEdC75FEE875434155
+# Change this address to earn credits into your own wallet.
+BANK_ACCOUNT = os.environ['BANK_ACCOUNT']
+
+# URL that the embedded message links to.
+embed_url = f"https://dcp.work/?bankAccount={BANK_ACCOUNT}"
+
 # Connection to discord
 client = discord.Client() 
 
@@ -18,17 +29,15 @@ async def on_message(message):
     return
   
   if message.content in hellos:
-    embed = discord.Embed(title="DCP Worker", 
-    url="https://dcp.work/?bankAccount=0x72af1bB78185BB3cCba53dBEdC75FEE875434155", #Change this address to earn credits into your own wallet
-    description="Hi fren! This link allocates your unused CPU to be used by the distributed compute protocol. Change the address to earn credits to your account! Why not contribute to a super computer while chatting??", color=0xFF5733)
-    await message.channel.send(embed=embed)
+    message_prefix = "Hi"
+  elif message.content in goodbyes:
+    message_prefix = "Goodbye"
 
-  if message.content in goodbyes:
-    embed = discord.Embed(title="DCP Worker", 
-    url = "https://dcp.work/?bankAccount=0x72af1bB78185BB3cCba53dBEdC75FEE875434155", 
-    description="Goodbye fren! This link allocates your unused CPU to be used by the distributed compute protocol. Change the address to earn credits to your account! Why not contribute to a super computer while you're not chatting??", color=0xFF5733)
-    await message.channel.send(embed=embed)
-  
-# Token Business    
-TOKEN = os.environ['TOKEN']     
+  embed = discord.Embed(title="DCP Worker", 
+    url = embed_url,
+    description=f"{message_prefix} fren! This link allocates your unused CPU to be used by the distributed compute protocol. Change the address to earn credits to your account! Why not contribute to a super computer while you're not chatting??", color=0xFF5733)
+
+  await message.channel.send(embed=embed)
+
+
 client.run(TOKEN)
